2be3c4732a19777ccea12f7427e364dc1474f860
Matthew Patterson <mdpatterson924@gmail.com>
2019-03-25 14:04:25 -0400

    Set up Restaurant index table

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDBContext db = new RestaurantDBContext();
         // GET: Restaurant
         public ActionResult Index()
         {
-            return View();
+            return View(db.Restaurants.ToList());
         }
     }
 }
